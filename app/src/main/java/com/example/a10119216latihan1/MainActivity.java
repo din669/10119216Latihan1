032e4d1d0ba11dd0dd3019f50c6c1ab13fad8774
din669 <muhammadsyarifuddin669@gmail.com>
2022-04-19 22:16:02 +0700

    origin

---
+++ app/src/main/java/com/example/a10119216latihan1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.a10119216latihan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+//Kelas  : IF-6
+//Nama   : Muhammad Syarifuddin Rahiman
+//NIM    : 10119216
